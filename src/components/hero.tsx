426a86800d4cf5dadaeb8babce0a4fe8a209bf73
Chris Ge <chrisge@dhcp-10-29-237-0.dyn.MIT.EDU>
2023-12-12 16:57:46 -0500

    changed legacy next.js image

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import headerbg from "../../public/header_bg.png";
 import styles from "./Hero.module.css"; // Assuming you're using CSS Modules
 
